28560adf1862bc178e89a01e74ca27670b92a6c1
yavnikaa <yavnikagarg3435@gmail.com>
2021-06-30 22:46:55 +0530

    Add websockets class

--- server-side/index.js
+++ server-side/index.js
@@ -3,10 +3,17 @@ import router from './routes'
 import database from './models';
 import dbConfig from './db-config.json'
 import config from 'config'
+import dotenv from 'dotenv'
+
 const app=express();
+const cors=require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+const PORT = process.env.PORT || 8080;
+
+dotenv.config();
 
 if (!config.get('PrivateKey')) {
     console.error('FATAL ERROR: PrivateKey is not defined.');
@@ -26,7 +33,33 @@ database.mongoose.connect(`mongodb+srv://${dbConfig.username}:${dbConfig.passwor
     process.exit();
 });
 
+const server = require("http").createServer(app);
+export const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
+}); 
+
 app.use('/api/v1/user',router.user)
 app.use('/api/v1/login', router.auth)
 
-app.listen(8080);
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
