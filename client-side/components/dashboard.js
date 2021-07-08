7e71fcfc5b8cf4ace465be2abf47c873cbcfc6cb
yavnikaa <yavnikagarg3435@gmail.com>
2021-07-08 23:51:28 +0530

    ADd dashboard design

--- client-side/components/dashboard.js
+++ client-side/components/dashboard.js
@@ -3,10 +3,13 @@ import { Redirect } from 'react-router'
 import { useHistory } from 'react-router'
 import axios from 'axios'
 import logout from '../utils/logout'
+
+import { Persona,  PersonaSize, Stack } from '@fluentui/react/lib';
+
 const Dashboard = ({ }) => {
     let history = useHistory();
     const [users, setUsers] = useState([])
-    const [selectedUser, setSelectedUser] = useState(null)
+    const [selectedUser, setSelectedUser] = useState([])
     const [selectedChat, setSelectedChat] = useState([])
     const token = localStorage.getItem('token')
     if (!token) {
@@ -22,14 +25,43 @@ const Dashboard = ({ }) => {
             },
             responseType: "json"
         }).then((res) => {
-            setUsers(res.data.users)
-            if (users.length > 0) setSelectedUser(users[0])
+                setUsers(res.data.users)
+                if (users.length > 0) setSelectedUser([users[0]])
         }).catch((error) => {
+            console.log(error)
+        }, []),
+        axios({
+            method:"GET",
+            url:'/api/v1/chat/messages/',
+            params: {
+                "recipient": `${selectedUser.username}`,
+            },
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            responseType: "json"
+        }).then((res) => {
+            if (selectedUser){
+                setSelectedChat(res.data)
+            }
+        }).catch ((error) => {
             console.log(error)
         }, [])
     });
     return (
-        <div>{JSON.stringify(users)}</div>
+        <div>
+            <Stack tokens={{ childrenGap: 20 }} >
+                {users.map((user,i)=> {
+                    return (
+                        <Persona key={i} text={users[i].username} size={PersonaSize.size48} secondaryText={users[i].bio} imageInitials={users[i].username[0]}
+                       value={selectedUser} onClick={(e) => setSelectedUser(e.target.value)} />
+                    )
+                })}
+            </Stack>
+            <div> {JSON.stringify(selectedChat)} </div>
+            
+        </div>
     )
 }
 export default Dashboard
